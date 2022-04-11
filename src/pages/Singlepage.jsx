8009702502600c6e1cb0ed25ea1c04163ecd3d06
Solomii <kushniryksolomiya@gmail.com>
2022-04-11 10:35:57 +0300

    [#0] Add post (tilte and body) to the Singlepage

--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Singlepage = () => {
-  const {id} = useParams();
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => res.json())
+      .then(data => setPost(data))
+
+  }, [id])
+
+
   return (
     <div>
-       {id}
-      <h1>Single Page</h1>
+      {post && (
+        <>
+           <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </>  
+      )}
     </div>
   )
 }
